52b23d9a9e82b26e5146513a4bb8cc0e2f7d8908
robertk <robertmakoto@mac.com>
2020-08-05 17:37:03 -0700

    Finished 'initialize' specs

--- rspec_homework/spec/dessert_spec.rb
+++ rspec_homework/spec/dessert_spec.rb
@@ -6,16 +6,25 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
-  let(:chef) { double("chef") }
+  let(:chef) { double("chef", name: "Jason") }
+  let(:cookie) { Dessert.new("cookie", 50, chef) }
 
   describe "#initialize" do
-    it "sets a type"
+    it "sets a type" do
+      expect(cookie.type).to eq("cookie")
+    end
 
-    it "sets a quantity"
+    it "sets a quantity" do
+      expect(cookie.quantity).to eq(50)
+    end
 
-    it "starts ingredients as an empty array"
+    it "starts ingredients as an empty array" do
+      expect(cookie.ingredients).to be_empty
+    end
 
-    it "raises an argument error when given a non-integer quantity"
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new("sundae", "pounds", chef) }.to raise_error(ArgumentError)
+    end
   end
 
   describe "#add_ingredient" do
